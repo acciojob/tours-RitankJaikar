06f154df8a6c8111f8e628cce79b3e8cbafabf77
RitankJaikar <ritank.jaikar@gmail.com>
2024-12-15 11:32:11 +0530

    first commit

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -14,7 +14,7 @@ function Tour({ id, name, info, image, price, removeTour }) {
         <p id={"tour-item-para-"+id}>
           {readMore ? info : `${info.substring(0, 200)}...`}
           <button onClick={() => setReadMore(!readMore)} id={"see-more-"+id}>
-            {readMore ? "Show less" : "Show more"}
+            {readMore ? "Show less" : "See more"}
           </button>
         </p>
         <button className="delete-btn" id={"delete-btn-"+id} onClick={() => removeTour(id)}>
